180736dd664dacce792ab0edbb15641d525035f6
Emma <146089189+emma-veingartner@users.noreply.github.com>
2024-01-12 16:47:44 +0000

    Updated the implementation of the forecast API

--- src/index.js
+++ src/index.js
@@ -54,6 +54,13 @@ function startSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "fo7d49b90da2ab24954c4563c144a1ta";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -65,15 +72,15 @@ function displayForecast(response) {
 
   let forecastHtml = "";
 
-  response.data.daily.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
   <div class="weather-forecast-day">
-    <div class="weather-forecast-date">Fri</div>
-    <div class="weather-forecast-icon">
-    <img src="${day.condition.icon_url}" />
-    </div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
     <div class="weather-forecast-temperatures">
       <div class="weather-forecast-temperature">
         <strong>${Math.round(day.temperature.maximum)}°</strong>
@@ -84,6 +91,7 @@ function displayForecast(response) {
     </div>
   </div>
 `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
